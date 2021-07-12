1747ab47ae149e27a736e27c267a84a891ea08ed
Jayfunf <simh3077@gmail.com>
2021-07-12 18:32:18 +0900

    init

---
+++ Music_App_Project_A/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music_App_Project_A
+//
+//  Created by Minhyun Cho on 2021/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
